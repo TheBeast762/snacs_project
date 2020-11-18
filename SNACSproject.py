53941a4cb59542ef71dc24ed0ce88aacf207011e
Damian Domela <damiandomela@Damians-MacBook-Air.local>
2020-11-18 15:47:49 +0100

    third commit

--- SNACSproject.py
+++ SNACSproject.py
@@ -1,3 +1,17 @@
+#ssh s1853767@sshgw.leidenuniv.nl
+#ssh huisuil01.vuw.leidenuniv.nl
+#ssh -K U0032504
+#git clone https://github.com/TheBeast762/snacs_project.git
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#brew install autoconf
+#brew install automake
+#brew install flex
+#brew install bison
+#brew install igraph
+#cd /vol/home/s1853767/miniconda3/compiler_compat
+#mv ld ld-old
+#python3 setup.py --use-pkg-config build
+#python3 setup.py --use-pkg-config install
 import louvain
 import igraph as ig
 
