e50ff71ab6fee9b5ce5557c095430899d98909a7
Damian Domela <damiandomela@Damians-MacBook-Air.local>
2020-11-23 16:58:46 +0100

    leaves bar 3

--- SNACSproject.py
+++ SNACSproject.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 	# 4 = RAND_NEIGH_COMM (Traag's Improved Method)
 	method_dict = {1: "ALL_COMMS", 2: "ALL_NEIGH_COMMS", 3: "RAND_COMM", 4:"RAND_NEIGH_COMM"}
 	settings_list = [(0.0, 2, False), (0.0, 2, True), (0.0, 4, False), (0.0, 4, True)]#threshold, comm_select, leaf_node_exclusion
-	networks = [readNetwork("medium.tsv")]#[readNetwork("rec-amazon.tsv"), readNetwork("soc-academia.tsv"), readNetwork("rt-higgs.tsv"), readNetwork("inf-roadNet-PA.tsv")]#100196, 125K, 200K, 425K, 1M
+	networks = [readNetwork("rec-amazon.tsv"), readNetwork("soc-academia.tsv"), readNetwork("rt-higgs.tsv"), readNetwork("inf-roadNet-PA.tsv")]#100196, 125K, 200K, 425K, 1M
 	q_dict = {}
 	t_dict = {}
 	leaves_amount = []
