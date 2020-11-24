4f60ba3dbd0342b1ab53198c94ec3bd5511f91a7
Damian Domela <damiandomela@Damians-MacBook-Air.local>
2020-11-24 23:30:29 +0100

    old method attempt

--- SNACSproject.py
+++ SNACSproject.py
@@ -22,43 +22,44 @@ def readNetwork(filename, directed=True):
 	print("reading {}...".format(filename))
 	return ig.Graph.Read_Ncol(open(filename), names=False, weights="if_present", directed=directed)
 
-def leafAdd(graph, partition, leafNodes):
-	t_start = time.time()
-	if not leafNodes:
-		return partition, 0.0
-	n_comm = len(partition._membership)
-	for leaf in leafNodes:
-		partition._membership.insert(leaf, n_comm)
-		n_comm += 1
+def leafPrune(graph):#{leafNode, Connection}
+  leafNodes = [v.index for v in graph.vs.select(_degree_eq=1)]#all nodes degree 1
+  if len(leafNodes) == 0:
+  	return [], [], 0
+  leafSources = [edge.tuple for edge in graph.es.select(_source_in=leafNodes)]#all edges connected to leaf nodes #leafEdgeData.attributes() if applicable
+  leafTargets = [edge.tuple for edge in graph.es.select(_target_in=leafNodes)]
+  graph.delete_edges(leafSources + leafTargets)
+  print("Pruned network size: ", graph.ecount())
+  return leafSources, leafTargets, len(leafNodes)
 
-	part = louvain.ModularityVertexPartition(graph, initial_membership=partition._membership)
-	leafSources = [edge.tuple for edge in graph.es.select(_source_in=leafNodes)]#all edges connected to leaf nodes #leafEdgeData.attributes() if applicable
-	leafTargets = [edge.tuple for edge in graph.es.select(_target_in=leafNodes)]
+def leafAdd(graph, partition, leafSources, leafTargets):
+	t_start = time.time()
+	graph.add_edges(leafSources + leafTargets)#reconnect edges to graph
+	partition = louvain.ModularityVertexPartition(graph, initial_membership=partition._membership)
 	for edge in leafSources:
-		part.move_node(edge[0], partition._membership[edge[1]])
+		partition.move_node(edge[0], partition._membership[edge[1]])
 	for edge in leafTargets:
-		part.move_node(edge[1], partition._membership[edge[0]])
+		partition.move_node(edge[1], partition._membership[edge[0]])
 	t_end = time.time()
-	part.renumber_communities()
-	return part, (t_end-t_start)
+	print("LeafAdd duration: ", t_end-t_start)
+	partition.renumber_communities()
+	return partition, (t_end-t_start)
 
 def performExperiment(G, threshold, comm_select, leafExclude):
 	print("Full network size: ", G.vcount(), G.ecount())
 	if leafExclude:
-		leaves = [v.index for v in G.vs.select(_degree_lt=2)]
-		subGraph = G.subgraph(vertices=G.vs.select(_degree_gt=1))
-		print("----- {} leafNodes found in the Network-----".format(len(leaves)))
+		leafSources, leafTargets, nLeaves = leafPrune(G)
+		print("----- {} leafNodes found in the Network-----".format(nLeaves))
 		t_start = time.time()
-		part = louvain.find_partition(subGraph, louvain.ModularityVertexPartition, threshold=threshold, comm_select=comm_select)
-		part, leafTime = leafAdd(G, part, leaves)
+		part = louvain.find_partition(G, louvain.ModularityVertexPartition, threshold=threshold, comm_select=comm_select)
+		part, leafTime = leafAdd(G, part, leafSources, leafTargets)
 		t_end = time.time()
-		print("Leafadd Duration: ", leafTime)
-		return part.quality(), (t_end-t_start), len(leaves), leafTime
+		return part.quality(), (t_end-t_start), leafTime
 	else: 
 		t_start = time.time()
 		part = louvain.find_partition(G, louvain.ModularityVertexPartition, threshold=threshold, comm_select=comm_select)
 		t_end = time.time()
-	return part.quality(), (t_end-t_start), 0, 0.0
+	return part.quality(), (t_end-t_start), 0.0
 
 if __name__ == "__main__":
 	#Community Select methods:
@@ -68,12 +69,11 @@ if __name__ == "__main__":
 	# 4 = RAND_NEIGH_COMM (Traag's Improved Method)
 	method_dict = {1: "ALL_COMMS", 2: "ALL_NEIGH_COMMS", 3: "RAND_COMM", 4:"RAND_NEIGH_COMM"}
 	settings_list = [(0.0, 2, False), (0.0, 2, True), (0.0, 4, False), (0.0, 4, True)]#threshold, comm_select, leaf_node_exclusion
-	networks = [readNetwork("rec-amazon.tsv"), readNetwork("soc-academia.tsv"), readNetwork("rt-higgs.tsv"), readNetwork("inf-roadNet-PA.tsv"), readNetwork("inf-netherlands_osm.tsv", False)]#
+	networks = [readNetwork("medium.tsv")]#[readNetwork("rec-amazon.tsv"), readNetwork("soc-academia.tsv"), readNetwork("rt-higgs.tsv"), readNetwork("inf-roadNet-PA.tsv"), readNetwork("inf-netherlands_osm.tsv", False)]#
 	n_settings = len(settings_list)
 	ind = np.arange(len(networks))
 	q_dict = {}
 	t_dict = {}
-	leaves_amount = []
 	network_sizes = []
 	nLeaves = 0
 
@@ -83,7 +83,7 @@ if __name__ == "__main__":
 		for setting in settings_list:
 			print("________________________________________")
 			print("LeafNodeExclusion used?: ", setting[2])
-			q, t, nLeaves, leafTime = performExperiment(network, setting[0], setting[1], setting[2])
+			q, t, leafTime = performExperiment(network, setting[0], setting[1], setting[2])
 			print(q,t)
 			if setting in q_dict:
 				q_dict[setting].append(q)
@@ -91,7 +91,6 @@ if __name__ == "__main__":
 			else:
 				q_dict[setting] = [q]
 				t_dict[setting] = [(t, leafTime)]
-		leaves_amount.append(nLeaves)
 	
 	print(q_dict)
 	print(t_dict)
