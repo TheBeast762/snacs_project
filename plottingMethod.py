bb16b0d55f98ccd711a47bf6138b1a7212d39bbb
Damian Domela <damiandomela@192.168.2.13>
2020-11-28 17:56:53 +0100

    5 nodes

--- plottingMethod.py
+++ plottingMethod.py
@@ -5,6 +5,7 @@ q_dict = #setting: [q, q, ...]
 t_dict = #setting: [(t, leafTime), (t, leafTime), ...]
 network_sizes = [91813, 200169, 425008, 1000005, 2216688, , 4194301]
 nLeaves = [19580, 18113, 282735, 0, 129231, , 54]
+method_dict = {1: "ALL_COMMS", 2: "ALL_NEIGH_COMMS", 3: "RAND_COMM", 4:"RAND_NEIGH_COMM"}
 
 #Plot Modularity
 fig, ax1 = plt.subplots()
@@ -13,7 +14,7 @@ ax1.set_ylabel('Q')
 for _, val in q_dict.items():
 	ax1.plot(network_sizes, val)
 	ax1.scatter(network_sizes, val)
-ax1.legend()
+ax1.legend([method_dict[setting[1]] for setting in q_dict.keys()])#
 
 plt.title("Community Select Method Q")
 plt.savefig('methodModularity.png')
@@ -26,7 +27,7 @@ ax1.set_ylabel('Time (s)')
 for _, val in t_dict.items():
 	ax1.plot(network_sizes, val)
 	ax1.scatter(network_sizes, val)
-ax1.legend()
+ax1.legend([method_dict[setting[1]] for setting in q_dict.keys()])#
 
 plt.title("Community Select Method Time (s)")
 plt.savefig('methodTime.png')
